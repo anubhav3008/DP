8c6b54df5842b2548e4544b1454bafc152678419
anubhav <anubhav.workemail@gmail.com>
2017-04-11 18:00:59 +0530

    Counting bits better solution

--- src/com/anubhav/leetcode/CountingBits.java
+++ src/com/anubhav/leetcode/CountingBits.java
@@ -3,9 +3,9 @@ package com.anubhav.leetcode;
 public class CountingBits {
 
 	public static void main(String[] args) {
-		int x[]=(new Solution().countBits(5));
+		int x[]=(new Solution().countBits(1));
 		for(int i : x){
-			System.out.print(i);
+			System.out.print(i+ " ");
 		}
 
 	}
@@ -14,21 +14,49 @@ public class CountingBits {
 class Solution {
     public int[] countBits(int num) {
         int ans[]=new int[num+1];
-        for(int i=0;i<=num;i++){
-        	if(i==0) ans[i]=0;
-        	else if(i==1) ans[i]=1;
-        	else{
-        		
-        		int lastPowOfTwo=(int) Math.floor(Math.log(i)/Math.log(2));
-        		int lastPowOfTwoVal=(int) Math.pow(2, lastPowOfTwo);
-        		if(i==lastPowOfTwoVal) ans[i]=1;
-        		else{
-        			ans[i]=ans[lastPowOfTwoVal]+ans[i-lastPowOfTwoVal];
-        		}
+        ans[0]=0;
+        int i=1;
+        while(i<num+1){
+        	ans[i]=1;
+        	i=i*2;
+        }
+        int valTilNow=2;
+        for(i=3;i<num+1;i++){
+        	if(ans[i]!=1){
+        		ans[i]=valTilNow;
         	}
-        	
+        	else
+        		valTilNow=i;
         }
         
+        for(i=3;i<num+1;i++){
+        	if(ans[i]!=1){
+        		ans[i]=1+ans[i-ans[i]];
+        	}
+        }
         return ans;
     }
 }
+
+/*0 = >0
+ * 1 => 1
+ * 01= > 1
+ * 10 = > 1
+ * 11 => 2
+ * 100=> 1
+ * 101=> 2
+ * 110 =>2
+ * 111=>3
+ * 1000=>1
+ * 1001=>2
+ * 1010=>2
+ * 1011=>3
+ * 1100=>2
+ * 
+ * 
+ * 		0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+ * i = {0,1,1,2,1,4,4,4,1,8, 8, 8 ,8, 8,8 
+ */
+
+
+
