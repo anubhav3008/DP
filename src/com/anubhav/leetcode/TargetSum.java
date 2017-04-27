e4e801a2f0ba010d832078cab9ebd91974cfaacc
anubhav <anubhav.workemail@gmail.com>
2017-04-27 07:20:45 +0530

    sucessfull Target sum

--- src/com/anubhav/leetcode/TargetSum.java
+++ src/com/anubhav/leetcode/TargetSum.java
@@ -3,12 +3,13 @@ package com.anubhav.leetcode;
 public class TargetSum {
 
 	public static void main(String[] args) {
-		int ans=new TargetSum().findTargetSumWays(new int[]{1,2,7,9,981}, 1000000000);
+		int ans=new TargetSum().findTargetSumWays(new int[]{0,0,0,0,0,0,0,0,1}, 1);
 		System.out.println(ans);
 	}
 	
+
     public int findTargetSumWays(int[] nums, int S) {
-		
+		if(S>1000) return 0;
     	int n=nums.length;
     	int totalSum=0;
     	for(int i=0;i<n;i++){
@@ -17,7 +18,6 @@ public class TargetSum {
     	if(((totalSum+S)%2)!=0) return 0;
     	
     	int sumReq=(totalSum+S)/2;
-    	System.out.println(sumReq);
     	if(sumReq <0){
     		int scale=-sumReq;
     		for(int i=0;i<n;i++){
@@ -26,32 +26,21 @@ public class TargetSum {
     	}
     	
     	
-    	
     	int dp[][]=new int[n+1][sumReq+1];
     	
-    	for(int i=0;i<=sumReq;i++) dp[0][sumReq]=0;
+
     	for(int i=0;i<=n;i++) dp[i][0]=1;
     	
     	
     	
     	
     	for(int i=1;i<=n;i++){
-    		for(int j=1;j<=sumReq;j++){
+    		for(int j=0;j<=sumReq;j++){
     			dp[i][j]=dp[i-1][j];
     			if(j>=nums[i-1])
     					dp[i][j]+=dp[i-1][j-nums[i-1]];
     		}
     	}
-    	
-    	
-    	for(int i=0;i<dp.length;i++){
-    		for(int j=0;j<dp[0].length;j++){
-    			System.out.print(dp[i][j]+" ");
-    		}
-    		System.out.println();
-    	}
-    	
-    	
     	return dp[n][sumReq];
         
     }
